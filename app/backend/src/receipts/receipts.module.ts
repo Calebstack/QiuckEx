b387ee4fd3964ca2a356a8b54f72001e3c299875
Caleb <oluseguncaleb6@gmail.com>
2026-09-05 16:31:18 +0400

    fix(ci): resolve failing checks for #957

--- app/backend/src/receipts/receipts.module.ts
+++ app/backend/src/receipts/receipts.module.ts
@@ -4,7 +4,7 @@
  * Location: app/backend/src/receipts/receipts.module.ts
  */
 
-import { Module } from '@nestjs/common';
+import { Module } from '@Nest/common';
 import { ReceiptsController } from './receipts.controller';
 import { ReceiptsService } from './receipts.service';
 import { ReceiptNormalizer } from './normalizers/receipt.normalizer';
